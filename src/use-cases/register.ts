d25ab9dc1007ded6c0bf297cf12464057ea52273
Islanilton <islanilton.rodrigues@gmail.com>
2023-07-18 13:39:12 -0300

    chore: create register use-case ande prisma users repository

---
+++ src/use-cases/register.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUserCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUserCaseRequest) {
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('User already exists')
+  }
+
+  const password_hash = await hash(password, 6)
+  const prismaUsersRepository = new PrismaUsersRepository()
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+}
