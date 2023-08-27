b9d3993f57af4aeca3213112d5565d68c3259c6a
Islanilton <islanilton.rodrigues@gmail.com>
2023-08-27 19:41:19 -0300

    feat: finished prisma check-in repository

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,61 @@
+import dayjs from 'dayjs'
+import { prisma } from '@/lib/prisma'
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async findById(id: string) {
+    return prisma.checkIn.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  findManyByUserId(userId: string, page: number) {
+    return prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+  }
+
+  findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+    return prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(),
+          lte: endOfTheDay.toDate(),
+        },
+      },
+    })
+  }
+
+  countByUserId(userId: string) {
+    return prisma.checkIn.count({
+      where: {
+        user_id: userId,
+      },
+    })
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    return prisma.checkIn.create({
+      data,
+    })
+  }
+
+  async save(checkIn: CheckIn) {
+    return prisma.checkIn.update({
+      where: {
+        id: checkIn.id,
+      },
+      data: checkIn,
+    })
+  }
+}
