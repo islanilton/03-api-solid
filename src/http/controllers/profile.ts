0d048515af4e24f7325d121412347b8a352dff7b
Islanilton <islanilton.rodrigues@gmail.com>
2023-08-30 23:35:19 -0300

    chore: create middleware verify token

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,10 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, replay: FastifyReply) {
   await request.jwtVerify()
   console.log(request.user.sub)
-  return replay.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+  return replay.status(200).send({ ...user, password_hash: undefined })
 }
